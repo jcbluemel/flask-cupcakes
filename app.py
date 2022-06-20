85c2bd52571fb5959a19c4b8e8cdea6612a766bd
Joel <jcbluemel@yahoo.com>
2022-06-20 13:57:58 -0700

    finished part 2

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 """Flask app for Cupcakes"""
-from flask import Flask, request, redirect, render_template
+from flask_debugtoolbar import DebugToolbarExtension
+from flask import Flask, request, redirect, render_template, jsonify
 from models import Cupcake, db, connect_db
 
 app = Flask(__name__)
@@ -11,47 +12,44 @@ app.config['SQLALCHEMY_ECHO'] = True
 connect_db(app)
 
 # debug tool bar
-from flask_debugtoolbar import DebugToolbarExtension
 app.config['SECRET_KEY'] = "SECRET!"
 debug = DebugToolbarExtension(app)
 
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 
-#TODO: add serialize function to our cupcake model
+
 @app.get('/api/cupcakes')
 def list_cupcakes():
-    # query.all all the cupcakes
-    # serialize data
-    #return jsonify(cupcakes=[{info}, {info}, ...])
 
+    cupcakes = Cupcake.query.all()
+    serialized = [c.serialize() for c in cupcakes]
 
-    # desserts = Dessert.query.all()
-    # serialized = [d.serialize() for d in desserts]
-
-    # return jsonify(desserts=serialized)
-    #list of somethings
-
-    return 42
+    return jsonify(cupcakes=serialized)
 
 
 @app.get('/api/cupcakes/<int:cupcake_id>')
-def get_cupcake():
+def get_cupcake(cupcake_id):
 
-    #query.get(cupcake_id) for single cupcake
-    # return jsonify(cupcake={info})
+    cupcake = Cupcake.query.get(cupcake_id)
+    serialized = cupcake.serialize()
 
-    # dessert = Dessert.query.get(dessert_id)
-    # serialized = dessert.serialize()
+    return jsonify(cupcake=serialized)
 
-    # return jsonify(dessert=serialized)
-
-    #single cupcake
-    return 42
 
 @app.post('/api/cupcakes')
 def create_cupcake():
 
-    #figure out how to pass data
+    new_cupcake = Cupcake(
+        flavor=request.json["flavor"],
+        size=request.json["size"],
+        rating=request.json["rating"],
+        image=request.json["image"],
+    )
 
-    # create new cupcake from body data
-    return 42
+    db.session.add(new_cupcake)
+    db.session.commit()
+
+    serialized = new_cupcake.serialize()
+
+    # Return w/status code 201 --- return tuple (json, status)
+    return (jsonify(cupcake=serialized), 201)
